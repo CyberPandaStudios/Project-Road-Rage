50d84cd0bc053b8433334a3a25f9371c0a724301
BMan1023 <brennan.tilyou2018@gmail.com>
2022-04-25 21:53:50 -0500

    Added sign functionality and Rear Camera

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    CursorLockMode lockMode;
     void Start()
     {
-        lockMode = CursorLockMode.Locked;
-        Cursor.lockState = lockMode;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
